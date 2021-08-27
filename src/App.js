8d810d791a53065caf8d1cdf74f64a49091e4241
Wilder Zambrana <jhonvalenz.wz@gmail.com>
2021-08-27 01:02:51 -0400

    Saludo + señor(a) + nombre (segun la edad + genero)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,41 @@
 const nombre = document.querySelector("#nombre-input");
 const edad = document.querySelector("#edad-input");
+const genero = document.querySelector("#genero-input");
 const form = document.querySelector("#saludador-form");
  
-form.addEventListener("submit", (event) => {
-  if(edad.value > 30) {
-    alert("Hola señor(a) " + nombre.value);
+/*form.addEventListener("submit", (event) => {
+  if(edad.value > 30 ) {
+    if(genero.value === "Male"){
+      alert("Hola señor " + nombre.value);
+    } else{
+      alert("Hola señora " + nombre.value);
+    }    
+  } else if(edad.value > 0){
+    if(genero.value === "Male"){
+      alert("Hola joven " + nombre.value);
+    } else{
+      alert("Hola señorita " + nombre.value);
+    }
   } else {
     alert("Hola " + nombre.value);
   }  
+});*/
+var title="";
+form.addEventListener("submit", (event) => {
+  if(edad.value > 30 ) {
+    if(genero.value === "Male"){
+      title="señor ";
+    } else{
+      title="señora ";
+    }    
+  } else if(edad.value > 0){
+    if(genero.value === "Male"){
+      title="joven ";
+    } else{
+      title="señorita ";
+    }
+  } else {
+    title="";
+  }  
+  alert("Hola " + title + nombre.value);
 });
