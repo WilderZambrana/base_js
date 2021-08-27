c2e84d244f4c335b8eb8b78219fde7e49c2cb3dd
Wilder Zambrana <jhonvalenz.wz@gmail.com>
2021-08-27 00:15:32 -0400

    Saludo + señor(a) + nombre (segun la edad)

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 const nombre = document.querySelector("#nombre-input");
+const edad = document.querySelector("#edad-input");
 const form = document.querySelector("#saludador-form");
-
+ 
 form.addEventListener("submit", (event) => {
-  alert("Hola " + nombre.value);
+  if(edad.value > 30) {
+    alert("Hola señor(a) " + nombre.value);
+  } else {
+    alert("Hola " + nombre.value);
+  }  
 });
